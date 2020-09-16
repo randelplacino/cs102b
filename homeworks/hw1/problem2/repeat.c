37aa3617c6565a4f32ac8176fcbfcff39bc65a3e
randelplacino <randel.placino@cooper.edu>
2020-09-15 20:32:00 -0400

    hw1 problem2 supporting files were committed

---
+++ homeworks/hw1/problem2/repeat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+      int count, limit;
+      count = 0;
+      limit = 3;
+      while (count < limit) {
+            printf("Cue the sun!\n");
+            count = count + 1;
+      }
+      return(0);
+}
